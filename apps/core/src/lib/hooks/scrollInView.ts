c1c7ff5210a2797da10e76011a63f0f21296e3f3
Adrian Insua <57523779+adrianiy@users.noreply.github.com>
2024-07-18 17:00:47 +0200

    Feat/scroll header (#54)
    * feat: move header on scroll
    
    * feat: use header scroll

---
+++ apps/core/src/lib/hooks/scrollInView.ts
@@ -0,0 +1,41 @@
+import { useEffect, useRef } from "react";
+import { useBeastStore } from "../stores/beast-store";
+import { Column } from "../common";
+
+export const useScrollInViewHook = (
+    options: Partial<{
+        column: Column;
+        onAnimationFrame: (translate: number) => void
+    }>
+) => {
+    const ref = useRef<HTMLDivElement>(null);
+    const reqAnimFrameNo = useRef<number>(0);
+    const [scrollElement] = useBeastStore((state) => [state.scrollElement]);
+
+    useEffect(() => {
+        const _handleScroll = (ev: Event) => () => {
+            if (!options.column) {
+                cancelAnimationFrame(reqAnimFrameNo.current)
+
+                return;
+            }
+
+            const transform = Math.min((scrollElement.scrollLeft - options.column.left) + 20, options.column.width - (ref.current?.clientWidth || 0) - 86);
+
+            if (transform > 0 && !options.column.final) {
+                options.onAnimationFrame?.(transform);
+            } else {
+                options.onAnimationFrame?.(0);
+            }
+        }
+
+        if (ref.current && options.column) {
+            scrollElement?.addEventListener('scroll', (ev) => {
+                reqAnimFrameNo.current = requestAnimationFrame(_handleScroll(ev))
+            });
+        }
+
+    }, [ref])
+
+    return [ref]
+}
