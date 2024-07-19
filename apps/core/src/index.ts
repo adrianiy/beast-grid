319a2578d016634b9e18430f5dfcfa7321023487
adrianiy <adrianiy@inditex.com>
2024-07-19 14:08:16 +0200

    feat: process calculated data

--- apps/core/src/index.ts
+++ apps/core/src/index.ts
@@ -1,3 +1,4 @@
+export * from './lib/utils/math';
 export * from './lib/core';
 export * from './lib/common';
 export * from './lib/stores/beast-store';
--- apps/core/src/lib/common/interfaces.ts
+++ apps/core/src/lib/common/interfaces.ts
@@ -1,6 +1,6 @@
 import { CSSProperties, ReactNode } from 'react';
 import { DragItem } from './../stores/dnd-store/store';
-import { AggregationType, ChangeType, FilterType, OperationType, PinType, SortType } from './enums';
+import { AggregationType, ChangeType, FilterType, MathType, Operation, OperationType, PinType, SortType } from './enums';
 import { EChartsCoreOption } from 'echarts';
 
 export interface Row {
@@ -296,3 +296,17 @@ export interface VisibilityChanges {
 export interface Changes extends Partial<SortChanges>, Partial<SwapChanges>, Partial<PivotChanges>, Partial<VisibilityChanges> { }
 
 export type OnChanges = (changeType: ChangeType, changes: Changes) => void;
+
+export interface MathCell {
+    type: MathType;
+    cell: string;
+}
+
+export type Operand = MathCell | Formula;
+
+export interface Formula {
+    left: Operand | null;
+    right: Operand | null;
+    operation: Operation | null;
+    type: MathType;
+}
