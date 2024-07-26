83bb15c5b5aafb05af5ebbbaef9571a35e70e5c9
adrianiy <adrianiy@inditex.com>
2024-07-26 13:17:09 +0200

    fix: scroll width

--- apps/core/src/lib/stores/grid-store/store.ts
+++ apps/core/src/lib/stores/grid-store/store.ts
@@ -136,7 +136,6 @@ export const createGridStore = <T>(
     theme: string,
     onChanges?: OnChanges
 ) => {
-    // TODO: initial table width is not correct
     const columns = getColumnsFromDefs(columnDefs, defaultColumnDef);
 
     const groupOrder = Object.values(columns)
