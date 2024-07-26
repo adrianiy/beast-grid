b5680fc0f2c84bf2f950ea3ddb343e9efd294a9b
adrianiy <adrianiy@inditex.com>
2024-07-26 11:48:23 +0200

    chore: remove todo

--- apps/core/src/lib/components/sidebar/sidebar.tsx
+++ apps/core/src/lib/components/sidebar/sidebar.tsx
@@ -67,7 +67,6 @@ export default function SideBar<T>({ config, theme, ...chartProps }: { config: B
         }
     }, [sideBarConfig, config.style?.maxHeight]);
 
-    // TODO: save only with changes
     const closeSidebar = () => {
         setSidebarConfig(null);
         setUseModal(false);
